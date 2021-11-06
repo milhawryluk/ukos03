f885a27cd3264f824e4b97ee5c172143c8bf183c
Łucja Białobrzeska <s26482@szuflandia.pjwstk.edu.pl>
2021-11-06 15:09:17 +0100

    Update Lucja

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
-#inclue <iostream>
+#include <iostream>
+
 
 int main() {
 
 int liczba1, liczba2, liczba3;
 float srednia;
 
-std::cout << "Podaj 3 liczby calkowite" << std::endl;
+std::cout << "Podaj 3 liczby calkowite, nieujemne" << std::endl;
 std::cin >> liczba1, liczba2, liczba3;
 
 srednia = (liczba1 + liczba2 + liczba3)/3;
 
-cout << "Srednia: " << srednia;
+cout << "Srednia z liczb to: " << srednia;
 
 return 0;
 }
