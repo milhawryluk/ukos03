ad323946b42594cea96fef2b19a79fda7a09ef49
Łucja Białobrzeska <s26482@szuflandia.pjwstk.edu.pl>
2021-11-06 15:22:24 +0100

    Piata liczba

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main() {
 
-int liczba1, liczba2, liczba3;
+int liczba1, liczba2, liczba3, liczba4,liczba5;
 float srednia;
 
 std::cout << "Podaj 3 liczby calkowite, nieujemne" << std::endl;
-std::cin >> liczba1, liczba2, liczba3;
+std::cin >> liczba1, liczba2, liczba3, liczba4, liczba5;
 
-srednia = (liczba1 + liczba2 + liczba3)/3;
+srednia = (liczba1 + liczba2 + liczba3,liczba4,liczba5)/5;
 
 cout << "Srednia z liczb to: " << srednia;
 
